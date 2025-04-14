5411f2beb672f8640fe7852e126d481993e624b7
cloud <543808004@qq.com>
2025-04-14 17:27:45 +0800

    3

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,215 +4,215 @@ Page({
     imageList: [], // 存储选择的图片
     isRecording: false, // 是否正在录音
     recordTime: 0, // 录音时长
-    recordTimer: null // 录音计时器
+    recordTimer: null, // 录音计时器
   },
 
-  onLoad: function(options) {
+  onLoad: function (options) {
     // Add cloud initialization check
     if (!wx.cloud) {
-      console.error('请使用基础库2.2.3或以上');
+      console.error("请使用基础库2.2.3或以上");
       wx.showToast({
-        title: '请使用基础库2.2.3或以上',
-        icon: 'none'
+        title: "请使用基础库2.2.3或以上",
+        icon: "none",
       });
       return;
     }
-    
+
     try {
       wx.cloud.init({
         env: "cloud1-7g8zvttq86cae3cc", // Make sure this matches your actual env ID
         traceUser: true,
       });
     } catch (e) {
-      console.error('云初始化失败:', e);
+      console.error("云初始化失败:", e);
     }
   },
 
-  chooseImage: function() {
+  chooseImage: function () {
     wx.chooseImage({
       count: 9 - this.data.imageList.length,
-      sizeType: ['original', 'compressed'],
-      sourceType: ['album', 'camera'],
+      sizeType: ["original", "compressed"],
+      sourceType: ["album", "camera"],
       success: (res) => {
         this.setData({
-          imageList: this.data.imageList.concat(res.tempFilePaths)
+          imageList: this.data.imageList.concat(res.tempFilePaths),
         });
-      }
+      },
     });
   },
 
-  deleteImage: function(e) {
+  deleteImage: function (e) {
     const index = e.currentTarget.dataset.index;
     const imageList = this.data.imageList;
     imageList.splice(index, 1);
     this.setData({ imageList });
   },
 
-  submitImageHomework: function() {
+  submitImageHomework: function () {
     if (this.data.imageList.length === 0) {
       wx.showToast({
-        title: '请至少选择一张图片',
-        icon: 'none'
+        title: "请至少选择一张图片",
+        icon: "none",
       });
       return;
     }
-    
+
     wx.showLoading({
-      title: '上传中...',
+      title: "提交中...",
     });
-    
+
     const uploadTasks = this.data.imageList.map((tempFilePath, index) => {
       const cloudPath = `homework-images/${Date.now()}-${index}.jpg`;
       return wx.cloud.uploadFile({
         cloudPath,
-        filePath: tempFilePath
+        filePath: tempFilePath,
       });
     });
-    
+
     Promise.all(uploadTasks)
-      .then(results => {
-        const fileIDs = results.map(res => res.fileID);
+      .then((results) => {
+        const fileIDs = results.map((res) => res.fileID);
         return wx.cloud.callFunction({
-          name: 'submitHomework',
+          name: "submitHomework",
           data: {
-            type: 'image',
+            type: "image",
             fileIDs: fileIDs,
-            time: new Date()
-          }
+            time: new Date(),
+          },
         });
       })
-      .then(res => {
+      .then((res) => {
         wx.hideLoading();
         wx.showToast({
-          title: '提交成功',
-          icon: 'success'
+          title: "提交成功",
+          icon: "success",
         });
         this.setData({ imageList: [] });
       })
-      .catch(err => {
+      .catch((err) => {
         wx.hideLoading();
         wx.showToast({
-          title: '提交失败',
-          icon: 'none'
+          title: "提交失败",
+          icon: "none",
         });
-        console.error('提交图片作业失败:', err);
+        console.error("提交图片作业失败:", err);
       });
   },
 
-  startRecord: function() {
+  startRecord: function () {
     // Add permission check
     wx.getSetting({
-      success: res => {
-        if (!res.authSetting['scope.record']) {
+      success: (res) => {
+        if (!res.authSetting["scope.record"]) {
           wx.authorize({
-            scope: 'scope.record',
+            scope: "scope.record",
             success: () => this._startRecording(),
             fail: () => {
               wx.showToast({
-                title: '需要录音权限',
-                icon: 'none'
+                title: "需要录音权限",
+                icon: "none",
               });
-            }
+            },
           });
         } else {
           this._startRecording();
         }
-      }
+      },
     });
   },
 
-  _startRecording: function() {
+  _startRecording: function () {
     this.setData({
       isRecording: true,
-      recordTime: 0
+      recordTime: 0,
     });
-    
+
     wx.startRecord({
-      success: res => {
+      success: (res) => {
         this.handleRecordSuccess(res.tempFilePath);
       },
-      fail: err => {
+      fail: (err) => {
         this.setData({
-          isRecording: false
+          isRecording: false,
         });
         wx.showToast({
-          title: '录音失败',
-          icon: 'none'
+          title: "录音失败",
+          icon: "none",
         });
-        console.error('录音失败:', err);
-      }
+        console.error("录音失败:", err);
+      },
     });
-    
+
     this.data.recordTimer = setInterval(() => {
       this.setData({
-        recordTime: this.data.recordTime + 1
+        recordTime: this.data.recordTime + 1,
       });
     }, 1000);
   },
 
-  stopRecord: function() {
+  stopRecord: function () {
     if (!this.data.isRecording) return;
-    
+
     wx.stopRecord();
     clearInterval(this.data.recordTimer);
     this.setData({
       isRecording: false,
-      recordTimer: null
+      recordTimer: null,
     });
   },
 
-  handleRecordSuccess: function(tempFilePath) {
+  handleRecordSuccess: function (tempFilePath) {
     wx.showLoading({
-      title: '上传中...',
+      title: "上传中...",
     });
-    
-    const cloudPath = 'voice-homework/' + Date.now() + '.mp3';
+
+    const cloudPath = "voice-homework/" + Date.now() + ".mp3";
     wx.cloud.uploadFile({
       cloudPath: cloudPath,
       filePath: tempFilePath,
-      success: res => {
+      success: (res) => {
         wx.cloud.callFunction({
-          name: 'submitHomework',
+          name: "submitHomework",
           data: {
-            type: 'voice',
+            type: "voice",
             fileID: res.fileID,
             duration: this.data.recordTime,
-            time: new Date()
+            time: new Date(),
           },
-          success: res => {
+          success: (res) => {
             wx.hideLoading();
             wx.showToast({
-              title: '提交成功',
-              icon: 'success'
+              title: "提交成功",
+              icon: "success",
             });
             this.setData({
-              recordTime: 0
+              recordTime: 0,
             });
           },
-          fail: err => {
+          fail: (err) => {
             wx.hideLoading();
             wx.showToast({
-              title: '提交失败',
-              icon: 'none'
+              title: "提交失败",
+              icon: "none",
             });
-            console.error('提交口语作业失败:', err);
-          }
+            console.error("提交口语作业失败:", err);
+          },
         });
       },
-      fail: err => {
+      fail: (err) => {
         wx.hideLoading();
         wx.showToast({
-          title: '上传失败: ' + (err.errMsg || '未知错误'),
-          icon: 'none'
+          title: "上传失败: " + (err.errMsg || "未知错误"),
+          icon: "none",
         });
-        console.error('上传录音文件失败:', err);
-      }
+        console.error("上传录音文件失败:", err);
+      },
     });
   },
 
-  onUnload: function() {
+  onUnload: function () {
     if (this.data.isRecording) {
       this.stopRecord();
     }
-  }
+  },
 });
